098c702cd65e8c4a0cf7d820e3ff75d44c63af2a
Brendon Roberto <brendon@brendonroberto.com>
2023-04-30 23:48:00 -0400

    test: get accountabilibot-api-e2e tests working
    * Currently requires the accountabilibot-api to be started manually before tests run
    * Ideas for solving this behavior:
        * https://github.com/nrwl/nx/issues/3748

--- packages/accountabilibot-api-e2e/src/accountabilibot-api/accountabilibot-api.spec.ts
+++ packages/accountabilibot-api-e2e/src/accountabilibot-api/accountabilibot-api.spec.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 describe('GET /', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/`);
+    it('should return a message', async () => {
+        const res = await axios.get(`/api`);
 
-    expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
-  });
+        expect(res.status).toBe(200);
+        expect(res.data).toEqual({
+            message: 'Welcome to accountabilibot-api!',
+        });
+    });
 });
